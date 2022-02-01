d4cb480d36128a053756588fd3f284da731a08bf
Roberto Araneda Espinoza <robaraneda@gmail.com>
2022-02-01 12:04:13 -0300

    add supabase

---
+++ function.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://ajqlopiiejntabesnibe.supabase.co'
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzcyNjQ5MywiZXhwIjoxOTU5MzAyNDkzfQ.IUb0KRdX6F-sZ1PC5CkUV4Sw0JgZwDzXGoT79EkF4EY";
+
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
